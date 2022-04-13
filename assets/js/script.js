cd1661c1ae18d7e0d1cda5edb274a0363c273fb8
ytyeoh <ytyeoh@users.noreply.github.com>
2022-04-13 10:15:16 +0800

    add padding to image

--- assets/js/script.js
+++ assets/js/script.js
@@ -194,9 +194,9 @@ https://github.com/imakewebthings/waypoints/blob/master/licenses.txt
       $('#color').html('<h3 class="">color</h3><div class="row color-img" ></div>');
       for (let i = 0; i < parseInt(this.id[1]); i++) {
         if (i==0){
-         $(".color-img").append('<a href="javascript:void(0);" class=" delete col-3"><img src="'+this.name+'/color/'+String(i+1)+'/1.png" class="img-active img-fluid img-replace" alt="'+this.id.substring(2).split(',')[i]+'" name= "'+this.name+'/color/'+String(i+1)+'/"></a>');
+         $(".color-img").append('<a href="javascript:void(0);" class=" delete col-3"><img src="'+this.name+'/color/'+String(i+1)+'/1.png" class=" p-2 img-active img-fluid img-replace" alt="'+this.id.substring(2).split(',')[i]+'" name= "'+this.name+'/color/'+String(i+1)+'/"></a>');
         } else {
-          $(".color-img").append('<a href="javascript:void(0);" class=" delete col-3"><img src="'+this.name+'/color/'+String(i+1)+'/1.png" class="img-fluid img-replace" alt="'+this.id.substring(2).split(',')[i]+'" name= "'+this.name+'/color/'+String(i+1)+'/"></a>');
+          $(".color-img").append('<a href="javascript:void(0);" class=" delete col-3"><img src="'+this.name+'/color/'+String(i+1)+'/1.png" class="p-2 img-fluid img-replace" alt="'+this.id.substring(2).split(',')[i]+'" name= "'+this.name+'/color/'+String(i+1)+'/"></a>');
         }
       }
     });
@@ -205,7 +205,7 @@ https://github.com/imakewebthings/waypoints/blob/master/licenses.txt
       $('#slider').html('<div class="owl-carousel owl-theme" data-slider-id="1"></div>');
       $('#thumb_slider').html('<div class="owl-thumbs" data-slider-id="1"></div>');
       for (let i = 0; i < parseInt(this.children[0].alt); i++) {
-         $(".owl-carousel").append('<img loading="lazy" src="'+this.children[0].name+'/'+String(i+1)+'.png" class="mt-5 card-img-top img-fluid" alt="vision-thumb">');  
+         $(".owl-carousel").append('<img loading="lazy" src="'+this.children[0].name+'/'+String(i+1)+'.png" class="p-10 mt-5 card-img-top img-fluid" alt="vision-thumb">');  
       }
       $('#color .img-active').removeClass('img-active');
       $(this).addClass('img-active');
@@ -216,7 +216,7 @@ https://github.com/imakewebthings/waypoints/blob/master/licenses.txt
       $('#slider').html('<div class="owl-carousel owl-theme" data-slider-id="1"></div>');
       $('#thumb_slider').html('<div class="owl-thumbs" data-slider-id="1"></div>');
       for (let i = 0; i < parseInt(this.alt); i++) {
-         $(".owl-carousel").append('<img loading="lazy" src="'+this.name+'/'+String(i+1)+'.png" class="mt-5 card-img-top img-fluid" alt="vision-thumb">');  
+         $(".owl-carousel").append('<img loading="lazy" src="'+this.name+'/'+String(i+1)+'.png" class="p-10 mt-5 card-img-top img-fluid" alt="vision-thumb">');  
       }
       $('#graphice-img .img-active').removeClass('img-active');
       $(this).addClass('img-active');
